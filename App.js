98fbd089843fc8c99d6e4b26310ded202ed96bd1
unknown <chohankoti333@gmail.com>
2023-12-30 15:57:02 +0530

    second

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import locationsData from './LocationData';
 
 export default function App() {
+  const INITIAL_REGION = {
+    latitude: 17.68,
+    longitude: 81.78,
+    latitudeDelta: 1,
+    longitudeDelta: 1,
+  };
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}/>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+      >
+        {locationsData.map((location, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
---
+++ LocationData.js
@@ -0,0 +1,75 @@
+const locationsData = [
+    {
+      name: "Mumbai",
+      latitude: 19.0760,
+      longitude: 72.8777,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Delhi",
+      latitude: 28.6139,
+      longitude: 77.2090,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Bangalore",
+      latitude: 12.9716,
+      longitude: 77.5946,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Chennai",
+      latitude: 13.0827,
+      longitude: 80.2707,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Kolkata",
+      latitude: 22.5726,
+      longitude: 88.3639,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Hyderabad",
+      latitude: 17.3850,
+      longitude: 78.4867,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Ahmedabad",
+      latitude: 23.0225,
+      longitude: 72.5714,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Pune",
+      latitude: 18.5204,
+      longitude: 73.8567,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Jaipur",
+      latitude: 26.9124,
+      longitude: 75.7873,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+    {
+      name: "Lucknow",
+      latitude: 26.8467,
+      longitude: 80.9462,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    },
+  ];
+  
+  export default locationsData;
+  
